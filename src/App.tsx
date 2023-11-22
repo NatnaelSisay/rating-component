155d40cd02d6cb50d86a8ed7891016400dca7d35
NatnaelSisay <natyjava8@gmail.com>
2023-11-22 13:13:16 -0500

    adding basic scss files and style

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "./App.scss";
 function App() {
 	return (
 		<>
-			<h1>Basic</h1>
+			<div className="app-container">
+				<div className="app-content">
+					<h1>Basic</h1>
+				</div>
+			</div>
 		</>
 	);
 }
