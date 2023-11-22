f46d3d814ffd2d8e36eb2eecda1c0d84d357ae11
NatnaelSisay <natyjava8@gmail.com>
2023-11-22 13:49:32 -0500

    initial non active mobile view draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import "./App.scss";
+import Star from "./assets/icon-star.svg";
 
 function App() {
 	return (
 		<>
 			<div className="app-container">
 				<div className="app-content">
-					<h1>Basic</h1>
+					<div className="card-rating">
+						<div className="star-container">
+							<img className="star" src={Star} alt="star icon" />
+						</div>
+
+						<h2>How did we do?</h2>
+						<p>
+							Please let us know how we did with your support request. All
+							feedback is appreciated to help us improve our offering!
+						</p>
+
+						<ul className="rating-values">
+							<li className="rating-value">1</li>
+							<li className="rating-value">2</li>
+							<li className="rating-value">3</li>
+							<li className="rating-value">4</li>
+							<li className="rating-value">5</li>
+						</ul>
+
+						<div className="btn-container">
+							<button className="btn">Submit</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
