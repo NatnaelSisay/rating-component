7ef9513f5ff20bc717e913302b4aa4206de1f2f3
NatnaelSisay <natyjava8@gmail.com>
2023-11-28 15:19:55 -0500

    refactored App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
 import "./App.scss";
-import Star from "./assets/icon-star.svg";
+
+import Card from "./components/card";
 
 function App() {
 	return (
 		<>
 			<div className="app-container">
 				<div className="app-content">
-					<div className="card-rating">
-						<div className="star-container">
-							<img className="star" src={Star} alt="star icon" />
-						</div>
-
-						<h2>How did we do?</h2>
-						<p>
-							Please let us know how we did with your support request. All
-							feedback is appreciated to help us improve our offering!
-						</p>
-
-						<ul className="rating-values">
-							<li className="rating-value">1</li>
-							<li className="rating-value">2</li>
-							<li className="rating-value">3</li>
-							<li className="rating-value">4</li>
-							<li className="rating-value">5</li>
-						</ul>
-
-						<div className="btn-container">
-							<button className="btn" data-testid="submit">
-								submit
-							</button>
-						</div>
-					</div>
+					<Card />
 				</div>
 			</div>
 		</>
